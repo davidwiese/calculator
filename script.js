2b518033081274a7e11883480d9b81350f633c5b
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-04-10 13:28:47 -0700

    Clean ID selectors

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+const btn4 = document.getElementById("btn-4");
+const btn5 = document.getElementById("btn-5");
+const btn6 = document.getElementById("btn-6");
+const btn7 = document.getElementById("btn-7");
+const btn8 = document.getElementById("btn-8");
+const btn9 = document.getElementById("btn-9");
+const btn0 = document.getElementById("btn-0");
+const btnAdd = document.getElementById("btn-add");
+const btnSubtract = document.getElementById("btn-subtract");
+const btnDivide = document.getElementById("btn-divide");
+const btnMultiply = document.getElementById("btn-multiply");
+const btnEquals = document.getElementById("btn-equals");
+const btnDot = document.getElementById("btn-dot");
+const display = document.getElementById("display");
+
+const btnClear = document.getElementById("btn-clear");
+btnClear.addEventListener("click", function () {
+  display.textContent = "";
+  firstNum = null;
+  secondNum = null;
+  operator = null;
+});
+
 function add(x, y) {
   return x + y;
 }
