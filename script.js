41ba89c9e95477e620be82bd43452d7e0f0f5b6f
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-04-10 15:30:01 -0700

    Clean up code and fix some edge case bugs

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let firstNum, secondNum, operator;
+//Global variables
+let firstNum, secondNum, operator, lastInputValue;
+let displayedValue = "";
+
+//DOM elements
 const btn1 = document.getElementById("btn-1");
 const btn2 = document.getElementById("btn-2");
 const btn3 = document.getElementById("btn-3");
@@ -15,29 +19,69 @@ const btnDivide = document.getElementById("btn-divide");
 const btnMultiply = document.getElementById("btn-multiply");
 const btnEquals = document.getElementById("btn-equals");
 const btnDot = document.getElementById("btn-dot");
-const display = document.getElementById("display");
 const btnClear = document.getElementById("btn-clear");
+const display = document.getElementById("display");
 
-btnClear.addEventListener("click", function () {
+//Functions
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function clearDisplay() {
   display.textContent = "";
+  displayedValue = "";
   firstNum = null;
   secondNum = null;
   operator = null;
-  displayedValue = "";
-});
+  lastInputValue = null;
+}
 
-btnEquals.addEventListener("click", function () {
-  if (firstNum && secondNum && operator) {
-    const result = operate(operator, firstNum, secondNum);
+function operate(operator, firstNum, secondNum) {
+  if (operator === "+") {
+    return add(firstNum, secondNum);
+  } else if (operator === "-") {
+    return subtract(firstNum, secondNum);
+  } else if (operator === "*") {
+    return multiply(firstNum, secondNum);
+  } else if (operator === "/") {
+    return divide(firstNum, secondNum);
+  } else {
+    return "ERROR";
+  }
+}
+
+function calculateResult() {
+  if (operator && lastInputValue) {
+    if (!secondNum) {
+      secondNum = Number(lastInputValue);
+    } else {
+      secondNum = operate(operator, secondNum, Number(lastInputValue));
+    }
+    operator = null;
+    lastInputValue = null;
+    const result = operate(previousOperator, firstNum, secondNum);
     display.textContent = result;
     firstNum = result;
     secondNum = null;
-    operator = null;
+    previousOperator = null;
   }
-});
+}
 
+//Event listeners
+document.getElementById("btn-clear").addEventListener("click", clearDisplay);
 const buttons = document.querySelectorAll(".calc-button");
-let displayedValue = "";
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const buttonValue = button.textContent;
@@ -60,62 +104,22 @@ buttons.forEach((button) => {
         firstNum = Number(displayedValue);
         displayedValue = "";
       }
+      lastInputValue = null;
     } else if (buttonValue === "=") {
-      secondNum = Number(displayedValue);
-      const result = operate(operator, firstNum, secondNum);
-      display.textContent = result;
-      displayedValue = result.toString();
-      operator = null;
-      firstNum = null;
-      secondNum = null;
-    } else if (buttonValue === "btn-clear") {
-      displayedValue = "";
-      operator = null;
-      firstNum = null;
-      secondNum = null;
-      display.textContent = "";
+      if (lastInputValue) {
+        secondNum = Number(lastInputValue);
+        const result = operate(operator, firstNum, secondNum);
+        display.textContent = result;
+        displayedValue = result.toString();
+        operator = null;
+        firstNum = null;
+        secondNum = null;
+        lastInputValue = null;
+      }
     } else {
       displayedValue += buttonValue;
       display.textContent = displayedValue;
+      lastInputValue = displayedValue;
     }
   });
 });
-
-function add(x, y) {
-  return x + y;
-}
-
-function subtract(x, y) {
-  return x - y;
-}
-
-function multiply(x, y) {
-  return x * y;
-}
-
-function divide(x, y) {
-  return x / y;
-}
-
-function clear() {
-  display.textContent = "";
-  firstNum = null;
-  secondNum = null;
-  operator = null;
-}
-
-document.getElementById("btn-clear").addEventListener("click", clear);
-
-function operate(operator, firstNum, secondNum) {
-  if (operator === "+") {
-    return add(firstNum, secondNum);
-  } else if (operator === "-") {
-    return subtract(firstNum, secondNum);
-  } else if (operator === "*") {
-    return multiply(firstNum, secondNum);
-  } else if (operator === "/") {
-    return divide(firstNum, secondNum);
-  } else {
-    return "ERROR";
-  }
-}
