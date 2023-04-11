d2633f7f7adfe6345794201c6e231345887e3897
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-04-11 09:57:53 -0700

    Remove unnecessary DOM selectors

--- script.js
+++ script.js
@@ -3,23 +3,6 @@ let firstNum, secondNum, operator, lastInputValue;
 let displayedValue = "";
 
 //DOM elements
-const btn1 = document.getElementById("btn-1");
-const btn2 = document.getElementById("btn-2");
-const btn3 = document.getElementById("btn-3");
-const btn4 = document.getElementById("btn-4");
-const btn5 = document.getElementById("btn-5");
-const btn6 = document.getElementById("btn-6");
-const btn7 = document.getElementById("btn-7");
-const btn8 = document.getElementById("btn-8");
-const btn9 = document.getElementById("btn-9");
-const btn0 = document.getElementById("btn-0");
-const btnAdd = document.getElementById("btn-add");
-const btnSubtract = document.getElementById("btn-subtract");
-const btnDivide = document.getElementById("btn-divide");
-const btnMultiply = document.getElementById("btn-multiply");
-const btnEquals = document.getElementById("btn-equals");
-const btnDot = document.getElementById("btn-dot");
-const btnClear = document.getElementById("btn-clear");
 const display = document.getElementById("display");
 
 //Functions
@@ -64,6 +47,7 @@ function operate(operator, firstNum, secondNum) {
 
 //Event listeners
 document.getElementById("btn-clear").addEventListener("click", clearDisplay);
+
 const buttons = document.querySelectorAll(".calc-button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
